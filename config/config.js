8e194520c3d3c3907bc67ebd2ab57fdf2b3d81c5
Kisame Holmes <adesinak4@gmail.com>
2024-07-08 08:21:20 +0100

    update

--- config/config.js
+++ config/config.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Adjust based on your SSL configuration
+      },
+    },
   },
   test: {
     username: process.env.DB_USERNAME,
@@ -16,10 +19,13 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Adjust based on your SSL configuration
+      },
+    },
   },
 };
