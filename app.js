b6179b3699c855eff9b0580faf5be2c37b37562b
hokageCodes <hashsenju94@gmail.com>
2024-07-11 16:46:10 +0100

    eyes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const organisationRoutes = require('./routes/organisationRoutes');
-const userRoutes = require('./routes/userRoute')
+const userRoutes = require('./routes/userRoute');
 const { sequelize } = require('./models');
 
 const app = express();
@@ -12,6 +12,11 @@ app.use(bodyParser.json());
 app.use('/auth', authRoutes);
 app.use('/api/organisations', organisationRoutes);
 app.use('/api/users', userRoutes);
+
+// Route to respond with "Hello World"
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
 
 sequelize.sync().then(() => {
   app.listen(port, () => {
